42e2b6ca8e756432f3e0b0e6199dbe1f9024f36a
Dave Corley <smokydave@icloud.com>
2023-09-29 03:12:12 -0500

    Fix(PacketActorAI::Actor): Read destination for follow packages

--- components/openmw-mp/Packets/Actor/PacketActorAI.cpp
+++ components/openmw-mp/Packets/Actor/PacketActorAI.cpp
@@ -24,7 +24,8 @@ void PacketActorAI::Actor(BaseActor &actor, bool send)
         if (actor.aiAction == mwmp::BaseActorList::ESCORT || actor.aiAction == mwmp::BaseActorList::WANDER)
             RW(actor.aiDuration, send);
 
-        if (actor.aiAction == mwmp::BaseActorList::ESCORT || actor.aiAction == mwmp::BaseActorList::TRAVEL)
+        if (actor.aiAction == mwmp::BaseActorList::ESCORT || actor.aiAction == mwmp::BaseActorList::TRAVEL ||
+            actor.aiAction == mwmp::BaseActorList::FOLLOW)
             RW(actor.aiCoordinates, send);
 
         if (actor.aiAction == mwmp::BaseActorList::ACTIVATE || actor.aiAction == mwmp::BaseActorList::COMBAT ||
